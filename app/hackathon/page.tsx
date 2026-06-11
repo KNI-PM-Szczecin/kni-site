861ed24a6403a2d1f5d649611fb4029a79894cc8
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-11 16:13:44 +0200

    Initalized page for hackathons

---
+++ app/hackathon/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+export default function HackathonPage() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex-1 pt-32 pb-20 bg-gray-50 dark:bg-gray-950 min-h-screen">
+            <h1>Hackathony - Wyjazdy gdzie kazdy moze sie sprawdzic </h1>
+      </main>
+    </>
+  );
+}
